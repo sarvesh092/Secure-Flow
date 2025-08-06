0fa1ea6298c4a5e281cb81aeda2e28991c435292
sarvesh <kashyapsarvesh092@gmail.com>
2025-08-06 16:51:43 +0530

    install react-redux and react toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,34 @@
 import FloatingCircles from "./component/floatingCircles";
+import Navigation from "./route";
+import store, { persistor } from "./store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "react-hot-toast";
+
 const App = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-fuchsia-900 to to-purple-500 flex items-center justify-center relative overflow-hidden">
-      <FloatingCircles
-        color="bg-fuchsia-900"
-        size="w-64 h-64"
-        top="-5%"
-        left="10%"
-        delay={0}
-      />
-      <FloatingCircles
-        color="bg-purple-900"
-        size="w-48 h-48"
-        top="70%"
-        left="80%"
-        delay={5}
-      />
-    </div>
+    <Provider store={store}>
+      <Toaster position="top-center" />
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="min-h-screen bg-gradient-to-br from-slate-900 via-sky-900 to-cyan-900 flex items-center justify-center relative overflow-hidden">
+          <FloatingCircles
+            color="bg-sky-900"
+            size="w-64 h-64"
+            top="-5%"
+            left="10%"
+            delay={0}
+          />
+          <FloatingCircles
+            color="bg-cyan-700"
+            size="w-48 h-48"
+            top="70%"
+            left="80%"
+            delay={5}
+          />
+          <Navigation />
+        </div>
+      </PersistGate>
+    </Provider>
   );
 };
 

0d53420eef9f046c982a31f7f3b53dbe91acf431
sarvesh <kashyapsarvesh092@gmail.com>
2025-08-09 22:50:37 +0530

    logout api integration

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import * as motion from "motion/react-client";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../store/action/auth-action";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error("Logout error in component:", error);
+    }
+  };
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <motion.button whileHover={{ scale: 1.1 }} onClick={handleLogout}>
+        Logout
+      </motion.button>
+    </div>
+  );
 };
 
 export default Dashboard;
